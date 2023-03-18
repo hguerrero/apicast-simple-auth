9fffb697f2da51b9d4201c70b0187aa3fa46c642
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2023-03-18 04:09:18 +0000

    move file read to init

--- policies/simple_auth/1.0.0/simple_auth.lua
+++ policies/simple_auth/1.0.0/simple_auth.lua
@@ -1,8 +1,8 @@
 local errors = require('apicast.errors')
 local resty_lrucache = require('resty.lrucache')
-local open = io.open
 
 local _M = require('apicast.policy').new('File Authorize', '1.0.0')
+
 local new = _M.new
 
 function _M.new(config)
@@ -17,13 +17,42 @@ function _M.new(config)
     self.auth_type = config.auth_type or 'none'
     -- self.auth_key = config.auth_key or 'user_key'
     self.keys_file = config.keys_file or '/opt/app/keys'
+    self.keys = {}
+    
+    if self.auth_key ~= 'none' then       
+      if config.keys ~= nil then 
+        for _,v in ipairs(config.keys) do self.keys[v] = v end
+      end
+      
+      local filename = self.keys_file
+      
+      -- load file with keys
+      local f = io.open(filename, "r")
+      if f then
+        f:close()
 
+        for line in io.lines(filename) do
+          self.keys[line] = line
+        end
+        ngx.log(ngx.INFO,'loaded additional keys from file: [', filename, '].')
+      else
+        ngx.log(ngx.WARN,'missing keys file: [', filename, '].')
+      end
+    end
+        
     self.cache = cache
 
     return self
 end
 
-function _M.rewrite(_, context)
+function _M:rewrite(context)
+    local auth_type = self.auth_type
+
+    if self.auth_type == 'none' then
+      context.extracted_credentials = {}
+      ngx.log(ngx.INFO, 'Provided default creds for request')
+    end
+
     context.skip_apicast_access = true
 end
 
@@ -88,45 +117,20 @@ function _M:authorize(context, service, usage, credentials, ttl)
         end
         -- local auth_key = self.auth_key
         local key = credentials['user_key'] or 'no key'
-
-        local filename = self.keys_file
-
+    
         ngx.log(ngx.NOTICE,'checking credentials: ', key)
-        
-        -- load file with keys
-        local f = io.open(filename, "r")
-        if f then
-          f:close()
-          
-          lines = {}
-          
-          for line in io.lines(filename) do
-            lines[line] = line
-          end
-        else
-          ngx.log(ngx.WARN,'missing keys file: [', filename, '] every request will be denied.')
+
+        local keys = self.keys
+
+        if not keys or next(keys) == nil then
+          ngx.log(ngx.WARN,'no keys available. every request will be denied.')
           return errors.authorization_failed(service)
         end
 
-        if not lines[key] then
-            return errors.authorization_failed(service)
+        if not keys[key] then
+          return errors.authorization_failed(service)
         end
       end
-
-    --   local backend = build_backend_client(self, service)
-    --   local res = backend:authrep(formatted_usage, credentials, self.extra_params_backend_authrep)
-
-    --   local authorized, rejection_reason, retry_after = self:handle_backend_response(
-    --     context, cached_key, res, ttl
-    --   )
-
-    --   if not authorized then
-    --     if rejection_reason == 'limits_exceeded' then
-    --       return errors.limits_exceeded(service, retry_after)
-    --     else -- Generic error for now. Maybe return different ones in the future.
-    --       return errors.authorization_failed(service)
-    --     end
-    --   end
     end
 end
 
